527ef0361be0416b2b45197607b0db0e4072c90e
sudrshan baliga <baliga108@gmail.com>
2019-09-18 17:46:31 +0530

    init

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <GL/glut.h>
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+}
+
+void init(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glPointSize(1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Flood rescue");
+    glutDisplayFunc(display);
+    init();
+    glutMainLoop();
+    return 0;
+}
