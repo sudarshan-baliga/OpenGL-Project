e6f65fc3610af35812dc3ba172d3fc3fee900105
sudrshan baliga <baliga108@gmail.com>
2019-09-19 22:59:12 +0530

    changed the layout

--- src/main.cpp
+++ src/main.cpp
@@ -6,31 +6,44 @@ void displayInfo()
 {
     float windowWidth, windowHeight, posX, posY, textWidth;
     int offsetX, offsetY;
-    char member1[] = "4NM16CS153 Sudarshan Baliga",
-         member2[] = "4NM16CS151 Srihari Vishnu",
-         member3[] = "4NM16CS152 Stalin Robo",
-         title[] = "Flood rescue simulation";
+    char usn1[] = "4NM16CS153",
+         usn2[] = "4NM16CS151",
+         usn3[] = "4NM16CS152",
+         name1[] = "SUDARSHAN BALIGA B",
+         name2[] = "SRIHARI VISHNU",
+         name3[] = "STALIN ROBO",
+         title[] = "RESCUE OPERATION USING HELICOPTER";
     //put the below code in separate thread for handling window size change
     windowWidth = glutGet(GLUT_WINDOW_WIDTH);
     windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
-    posX = windowWidth / 2;
-    posY = windowHeight / 2;
-    offsetY = 25;
-    glColor3f(1.0, 1.0, 1.0);
-    offsetX = glutBitmapLength(GLUT_BITMAP_HELVETICA_18, (unsigned char *)member1);
-    draw_text(member1, posX - offsetX / 2, posY, GLUT_BITMAP_HELVETICA_18);
+    posX = windowWidth / 2 - 150;
+    posY = windowHeight / 2 + 60;
+    offsetY = 30;
+
+    glColor3ub(91, 100, 249);
+    offsetX = glutBitmapLength(GLUT_BITMAP_HELVETICA_18, (unsigned char *)usn1) + 40;
+    draw_text(usn1, posX, posY, GLUT_BITMAP_HELVETICA_18);
+    glColor3ub(0, 183, 78);
+    draw_text(name1, posX + offsetX, posY, GLUT_BITMAP_HELVETICA_18);
     posY -= offsetY;
 
-    offsetX = glutBitmapLength(GLUT_BITMAP_HELVETICA_18, (unsigned char *)member2);
-    draw_text(member2, posX - offsetX / 2, posY, GLUT_BITMAP_HELVETICA_18);
+    glColor3ub(91, 100, 249);
+    offsetX = glutBitmapLength(GLUT_BITMAP_HELVETICA_18, (unsigned char *)usn2) + 40;
+    draw_text(usn2, posX, posY, GLUT_BITMAP_HELVETICA_18);
+    glColor3ub(0, 183, 78);
+    draw_text(name2, posX + offsetX, posY, GLUT_BITMAP_HELVETICA_18);
     posY -= offsetY;
 
-    offsetX = glutBitmapLength(GLUT_BITMAP_HELVETICA_18, (unsigned char *)member3);
-    draw_text(member3, posX - offsetX / 2, posY, GLUT_BITMAP_HELVETICA_18);
+    glColor3ub(91, 100, 249);
+    offsetX = glutBitmapLength(GLUT_BITMAP_HELVETICA_18, (unsigned char *)usn3) + 40;
+    draw_text(usn3, posX, posY, GLUT_BITMAP_HELVETICA_18);
+    glColor3ub(0, 183, 78);
+    draw_text(name3, posX + offsetX, posY, GLUT_BITMAP_HELVETICA_18);
     posY -= offsetY;
 
+   glColor3ub(91, 100, 249);
     offsetX = glutBitmapLength(GLUT_BITMAP_TIMES_ROMAN_24, (unsigned char *)title);
-    draw_text(title, posX - offsetX / 2, posY, GLUT_BITMAP_TIMES_ROMAN_24);
+    draw_text(title, posX - 60, posY - 50, GLUT_BITMAP_TIMES_ROMAN_24);
 }
 
 void display(void)
@@ -50,19 +63,20 @@ void on_resize(int w, int h)
 
 void init(void)
 {
-    glClearColor(0, 0.882, 0.827, 1);
+    // glClearColor(0, 0.882, 0.827, 1);
+    glClearColor(0, 0, 0, 1);
     glColor3f(1.0, 0.0, 0.0);
     glPointSize(1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+    gluOrtho2D(0.0, 599.0, 0.0, 599.0);
 }
 
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
+    glutInitWindowSize(600, 600);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("Flood rescue");
     glutDisplayFunc(display);
